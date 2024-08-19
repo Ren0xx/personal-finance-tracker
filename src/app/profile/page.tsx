7707935330e69909cb87e2594827c1fdec7095db
Ren0xx <adamzaleski00@gmail.com>
2024-08-19 13:17:00 +0200

    feat(ui): Changes in profile section

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,8 +5,6 @@ import { notFound } from "next/navigation";
 import { H2 } from "@/components/ui/typography";
 import DeleteWithConfirmForm from "@/components/forms/DeleteWithConfirmForm";
 import { deleteAccount, deleteAllUserData } from "@/server/actions/delete";
-import { Button } from "@/components/ui/button";
-import { signOut } from "next-auth/react";
 import LogOutButton from "@/components/LogOut";
 import { ThemeToggle } from "@/components/ThemeToggle";
 
@@ -16,19 +14,20 @@ export default async function Profile() {
     return notFound();
   }
   return (
-    <div className="grid grid-cols-2 gap-4">
+    <div className="flex grow flex-col gap-4 bg-card p-4 rounded-md">
       <section className="col-span-1 flex flex-col gap-2 ">
         <ProfileShowcase name={user.name} image={user.image} />
         <UpdateNameForm currentUserName={user.name ?? ""} />
         <hr />
-        <LogOutButton />
       </section>
 
-      <section className="col-span-1 flex flex-col gap-2">
+      <section className="col-span-1 flex grow flex-col items-center gap-2">
         <ThemeToggle />
         <H2> Dangerous Section</H2>
         <DeleteWithConfirmForm action={deleteAccount} title="Account" />
         <DeleteWithConfirmForm action={deleteAllUserData} title="All data" />
+        <hr />
+        <LogOutButton />
       </section>
     </div>
   );
